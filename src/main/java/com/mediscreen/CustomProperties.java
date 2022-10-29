c734d83e520a15bb562767b9a15304d3c8eeef82
Ranjinie-Souria <ranjinie.souria@gmail.com>
2022-10-29 19:01:13 +0200

    Added templates for patients Added application properties file Added pom.xml Added homecontroller

---
+++ src/main/java/com/mediscreen/CustomProperties.java
@@ -0,0 +1,22 @@
+package com.mediscreen;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "com.mediscreen")
+public class CustomProperties {
+
+    private String apiUrl;
+
+	public String getApiUrl() {
+		return apiUrl;
+	}
+
+	public void setApiUrl(String apiUrl) {
+		this.apiUrl = apiUrl;
+	}
+    
+    
+
+}
---
+++ src/main/java/com/mediscreen/controllers/HomeController.java
@@ -0,0 +1,11 @@
+package com.mediscreen.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+}
